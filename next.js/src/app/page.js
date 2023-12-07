f3c12253228f1772cd5859b441e3160da8a637d4
Bogumił <hello@milewski.me>
2023-12-07 14:52:57 +0100

    added Header

--- next.js/src/app/page.js
+++ next.js/src/app/page.js
@@ -13,7 +13,9 @@ const IndexPage = async () => {
     <>
       <Breadcrumbs data={[
         { name: 'Homepage', path: pathname },
-      ]} />
+      ]} visible={false} />
+      <h1>Hej</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro, obcaecati. Velit sit adipisci exercitationem ducimus deleniti eveniet, saepe qui repudiandae dignissimos vel molestiae ipsum nulla rerum minus pariatur veritatis tenetur natus. Ea, eaque eius. Illo labore possimus ab dolorum commodi quae, eligendi dignissimos, modi dolor pariatur at nisi adipisci alias omnis quisquam eveniet itaque corporis! Voluptas, et expedita ducimus distinctio, exercitationem ipsa nihil aspernatur quos, libero laboriosam impedit eaque incidunt possimus veritatis commodi! Ipsam suscipit perspiciatis dolores nemo odit. Ipsa eum illo deleniti aspernatur suscipit quis veritatis totam quia voluptatibus, a atque beatae assumenda nam sint dicta consequuntur facilis officia.</p>
     </>
   )
 }

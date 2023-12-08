8021ad94c390ce564841d6e3df68832722f4c373
Bogumił <hello@milewski.me>
2023-12-08 13:59:45 +0100

    added Input

---
+++ next.js/src/components/atoms/Input/index.js
@@ -0,0 +1,37 @@
+import styles from './styles.module.scss';
+import Error from '../Error';
+
+const Input = ({
+  register,
+  label,
+  errors,
+  textarea=false,
+  ...props
+}) => {
+  return (
+    <label
+      className={styles.wrapper}
+      aria-invalid={Boolean(errors[register.name])}
+    >
+      <p dangerouslySetInnerHTML={{ __html: label}} />
+      {textarea ? (
+        <textarea
+          data-lenis-prevent
+          rows={3}
+          {...register}
+          name={register.name}
+          {...props}
+        />
+      ) : (
+        <input
+          {...register}
+          name={register.name}
+          {...props}
+        />
+      )}
+      <Error error={errors[register.name]} />
+    </label>
+  );
+};
+
+export default Input;

5a980efacb119121fe6e138c93bdb24c9484404e
Bogumił <hello@milewski.me>
2023-12-11 16:08:48 +0100

    globally changed easings and box-sizing for Radio atom

--- next.js/src/global/constants.js
+++ next.js/src/global/constants.js
@@ -2,4 +2,4 @@ export const regex = {
   email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
   phone: /^(?:\+(?:\d{1,3}))?(?:[ -]?\(?\d{1,4}\)?[ -]?\d{1,5}[ -]?\d{1,5}[ -]?\d{1,6})$/,
 }
-export const easing = [.77,0,.18,1];
+export const easing = [.65,.05,.36,1];

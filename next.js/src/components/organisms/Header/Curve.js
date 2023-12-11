d625393345a62cd49401ec15c3b07082417f7ded
Bogumił <hello@milewski.me>
2023-12-11 15:31:04 +0100

    fix window is not accessible on SSR

--- next.js/src/components/organisms/Header/Curve.js
+++ next.js/src/components/organisms/Header/Curve.js
@@ -1,24 +1,29 @@
 'use client'
+import { useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 import styles from './styles.module.scss';
 import { easing } from '@/global/constants';
 
 const Curve = ({ open }) => {
-  const initialPath = `M100 0 L200 0 L200 ${window.innerHeight} L100 ${window.innerHeight} Q-100 ${window.innerHeight / 2} 100 0`
-  const targetPath = `M100 0 L200 0 L200 ${window.innerHeight} L100 ${window.innerHeight} Q100 ${window.innerHeight / 2} 100 0`
+  const initialPathRef = useRef('');
+  const targetPathRef = useRef('');
+  useEffect(() => {
+    initialPathRef.current = `M100 0 L200 0 L200 ${window.innerHeight} L100 ${window.innerHeight} Q-100 ${window.innerHeight / 2} 100 0`;
+    targetPathRef.current = `M100 0 L200 0 L200 ${window.innerHeight} L100 ${window.innerHeight} Q100 ${window.innerHeight / 2} 100 0`;
+  }, []);
+
   return (
     <svg className={styles.curve} stroke='none'>
       <motion.path
         initial={{
-          d: initialPath
+          d: initialPathRef.current
         }}
         animate={{
-          d: open ? targetPath : initialPath,
+          d: open ? targetPathRef.current : initialPathRef.current,
           transition: { duration: .8, ease: easing }
         }}
         exit={{
-          d: initialPath,
-          transition: { duration: 0.8, ease: easing }
+          d: initialPathRef.current,
         }}
       />
     </svg>

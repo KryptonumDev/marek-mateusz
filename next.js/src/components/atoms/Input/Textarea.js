63b0a159fc7b420f6eafdc6856a243843b686c90
Bogumił <hello@milewski.me>
2023-12-12 14:12:12 +0100

    small naming changes

--- next.js/src/components/atoms/Input/Textarea.js
+++ next.js/src/components/atoms/Input/Textarea.js
@@ -1,14 +1,14 @@
 'use client';
 
 const Textarea = ({ ...props }) => {
-  const autoExpandTextArea = (e) => {
+  const handleExpand = (e) => {
     const textarea = e.target;
     textarea.style.height = 'auto';
     textarea.style.height = `${textarea.scrollHeight}px`;
   }
 
   return (
-    <textarea onInput={autoExpandTextArea} {...props} />
+    <textarea onInput={handleExpand} {...props} />
   );
 };
 
